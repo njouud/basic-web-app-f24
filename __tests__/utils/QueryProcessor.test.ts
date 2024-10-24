8326d185a7c23d6523d448499b5a5e8060689b2f
Najoud <ntalib@andrew.cmu.edu>
2024-10-24 16:29:56 +0300

    changing to id

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -31,7 +31,7 @@ describe("QueryProcessor", () => {
         const query = "what is your name?";
         const response: string = QueryProcessor(query);
         expect(response).toBe((
-            "najoud"
+            "ntalib"
           ));
     });
 });
