8bfa81a24861534846d4062c9bf1f3a02684683c
Najoud <ntalib@andrew.cmu.edu>
2024-10-24 16:46:37 +0300

    plus q

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -16,9 +16,19 @@ export default function QueryProcessor(query: string): string {
   if (query.toLowerCase().includes("name")) {
     return ( "ntalib" );
   }
-  if (query.toLowerCase().includes("70 plus 67")) {
-    const num = 70 + 67;
-    return ( num.toString() );
+  if (query.toLowerCase().includes("plus")) {
+   // const num = 70 + 67;
+    // return ( num.toString() );
+    let numbers = query.match(/\d+/g); // Extracts all numbers from the string
+
+    if (numbers) {
+      let num1 = parseInt(numbers[0], 10);
+      let num2 = parseInt(numbers[1], 10);
+      console.log(num1, num2);
+
+      let ans = num1 + num2;
+      return ( ans.toString() );
+    }
   }
   
   return "";
